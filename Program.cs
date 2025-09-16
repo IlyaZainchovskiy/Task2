7b391221210a1b9da8e96f2e7d2e313bd42707a1
IlyaZainchovskiy <zainchkovskiy1@gmail.com>
2025-09-16 23:37:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintEdition edition = new PrintEdition();
+
+            edition.SetValues("Внутрішній", "Наказ", 2);
+
+            edition.ShowType();
+        }
+    }
+
+    class PrintEdition
+    {
+        private string type;
+        private string name;
+        private int pages;
+        public string Type
+        {
+            get => type;
+            set => type = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Pages
+        {
+            get => pages;
+            set => pages = value;
+        }
+
+        public PrintEdition()
+        {
+        }
+
+        public void SetValues(string t, string n, int p)
+        {
+            Type = t;
+            Name = n;
+            Pages = p;
+
+            Console.WriteLine($"Тип: {Type}, Назва: {Name}, Кількість сторінок: {Pages}");
+        }
+        public void ShowType()
+        {
+            Console.WriteLine($"Тип видання: {Type}");
+        }
+    }
+}
